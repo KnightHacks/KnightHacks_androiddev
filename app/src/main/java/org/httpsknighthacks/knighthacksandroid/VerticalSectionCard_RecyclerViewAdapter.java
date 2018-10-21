9992f375afc2603c8cef7217350ac28ad72d615a
krabinowitz1 <rabinowitzkevin@gmail.com>
2018-10-20 22:01:46 -0400

    Update VerticalSectionCard_RecyclerViewAdapter.java

--- app/src/main/java/org/httpsknighthacks/knighthacksandroid/VerticalSectionCard_RecyclerViewAdapter.java
+++ app/src/main/java/org/httpsknighthacks/knighthacksandroid/VerticalSectionCard_RecyclerViewAdapter.java
@@ -8,7 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -20,47 +19,39 @@ public class VerticalSectionCard_RecyclerViewAdapter extends RecyclerView.Adapte
     private ArrayList<String> mCardImageList;
     private ArrayList<String> mCardTitleList;
     private ArrayList<String> mCardSubtitleList;
+    private ArrayList<String> mCardDetailsList;
+    private String tag;
     private Context mContext;
-    private boolean[] isVisible;
 
     public VerticalSectionCard_RecyclerViewAdapter(Context mContext,
                                                      ArrayList<String> mCardImageList,
                                                      ArrayList<String> mCardTitleList,
-                                                     ArrayList<String> mCardSubtitleList) {
+                                                     ArrayList<String> mCardSubtitleList,
+                                                     ArrayList<String> mCardDetailsList,
+                                                     String tag) {
         this.mContext = mContext;
         this.mCardImageList = mCardImageList;
         this.mCardTitleList = mCardTitleList;
         this.mCardSubtitleList = mCardSubtitleList;
-        isVisible = new boolean[mCardSubtitleList.size()];
+        this.mCardDetailsList = mCardDetailsList;
+        this.tag = tag;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.vertical_section_card, parent, false));
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.vertical_section_card, parent, false), tag);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
         // Only load parts of the generic vertical card if needed
+
         if (position < mCardImageList.size()) {
-
-            // Use the resource id to determine what drawables (if any) need to load
-            int id = mContext.getResources().
-                    getIdentifier(mCardImageList.get(position), "drawable", mContext.getPackageName());
-
-            // The FAQ cards will have some differences in UI functionality since it needs to expand and collapse
-            if(mCardImageList.get(position).equals("ic_faq_plus")) {
-                setUpFaqIcon(id, holder, position);
-            }
-
-            else {
-                Glide.with(mContext)
-                        .asBitmap()
-                        .load(mCardImageList.get(position))
-                        .into(holder.mCardImage);
-            }
-
+            Glide.with(mContext)
+                    .asBitmap()
+                    .load(tag.equals(FAQs.TAG) ? getDrawableId(mCardImageList.get(position)) : mCardImageList.get(position))
+                    .into(holder.mCardImage);
         } else {
             holder.mCardImage.setVisibility(View.GONE);
         }
@@ -77,38 +68,17 @@ public class VerticalSectionCard_RecyclerViewAdapter extends RecyclerView.Adapte
             holder.mCardSubtitle.setVisibility(View.GONE);
         }
 
-
+        if (position < mCardDetailsList.size()) {
+            holder.mCardDetails.setText(mCardDetailsList.get(position));
+        } else {
+            holder.mCardDetails.setVisibility(View.GONE);
+        }
     }
 
-    private void setUpFaqIcon(int id, final ViewHolder holder, int position) {
-        // Without this, the answers will display upon starting the activity, which we don't want
-        if(!isVisible[position])
-        {
-            holder.mCardSubtitle.setVisibility(View.GONE);
-            isVisible[position] = true;
-        }
-        Glide.with(mContext)
-                .asBitmap()
-                .load(id)
-                .into(holder.mCardImage);
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(120, 120);
-        holder.mCardImage.setLayoutParams(params);
+    private int getDrawableId(String imageName) {
 
-        // Expand or collapse card when icon is pressed
-        holder.mCardImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(holder.mCardSubtitle.getVisibility() == View.GONE) {
-                    holder.mCardSubtitle.setVisibility(View.VISIBLE);
-                    holder.mCardImage.setImageDrawable(mContext.getResources().getDrawable(R.drawable.ic_faq_minimize));
-                }
-
-                else {
-                    holder.mCardSubtitle.setVisibility(View.GONE);
-                    holder.mCardImage.setImageDrawable(mContext.getResources().getDrawable(R.drawable.ic_faq_plus));
-                }
-            }
-        });
+        return mContext.getResources().getIdentifier(imageName,
+                "drawable", mContext.getPackageName());
     }
 
     @Override
@@ -116,18 +86,41 @@ public class VerticalSectionCard_RecyclerViewAdapter extends RecyclerView.Adapte
         return Math.max(mCardImageList.size(), Math.max(mCardTitleList.size(), mCardSubtitleList.size()));
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder{
         CardView mCardView;
         ImageView mCardImage;
         TextView mCardTitle;
         TextView mCardSubtitle;
+        TextView mCardDetails;
+        String mTag;
 
-        public ViewHolder(View itemView) {
+        public ViewHolder(View itemView, String tag) {
             super(itemView);
             this.mCardView = itemView.findViewById(R.id.vertical_section_card_view);
             this.mCardImage = itemView.findViewById(R.id.vertical_section_card_image);
             this.mCardTitle = itemView.findViewById(R.id.vertical_section_card_title);
             this.mCardSubtitle = itemView.findViewById(R.id.vertical_section_card_subtitle);
+            this.mCardDetails = itemView.findViewById(R.id.vertical_section_card_detail);
+            this.mTag = tag;
+
+            if(this.mTag.equals(FAQs.TAG))
+                this.mCardDetails.setVisibility(View.GONE);
+                this.mCardImage.setImageDrawable(mContext.getResources().getDrawable(R.drawable.ic_faq_plus_icon));
+                mCardView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(mCardDetails.getVisibility() == View.GONE) {
+                            mCardDetails.setVisibility(View.VISIBLE);
+                            mCardImage.setImageDrawable(mContext.getResources().getDrawable(R.drawable.ic_faq_minus_icon));
+                        }
+
+                        else {
+                            mCardDetails.setVisibility(View.GONE);
+                            mCardImage.setImageDrawable(mContext.getResources().getDrawable(R.drawable.ic_faq_plus_icon));
+                        }
+                    }
+                });
         }
     }
+
 }
