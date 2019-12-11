988622eeccd08a7e795d1fdafc46ece7baaabf35
Kevin Rabinowitz <rabinowitzkevin@gmail.com>
2019-12-10 22:52:43 -0500

    Update LiveUpdate.java

--- app/src/main/java/org/httpsknighthacks/knighthacksandroid/Models/LiveUpdate.java
+++ app/src/main/java/org/httpsknighthacks/knighthacksandroid/Models/LiveUpdate.java
@@ -6,12 +6,17 @@ import org.json.JSONObject;
 
 public class LiveUpdate {
 
+    public static final String MESSAGE_KEY = "message";
+    public static final String TIME_SENT_KEY = "timeSent";
+    public static final String PICTURE_KEY = "picture";
+
     private String message;
     private String picture;
-    private String optionalImage;
     private Timestamp timeSent;
 
-    public LiveUpdate() { }
+    public LiveUpdate() {
+
+    }
 
     public String getMessage() {
         return message;
@@ -20,7 +25,7 @@ public class LiveUpdate {
     public void setMessage(String message) {
         this.message = message;
     }
-  
+
     public String getPicture() {
         return picture;
     }
@@ -35,5 +40,11 @@ public class LiveUpdate {
 
     public void setTimeSent(Timestamp timeSent) {
         this.timeSent = timeSent;
+    }
+
+    public static boolean isValid(LiveUpdate update) {
+        return update.getMessage() != null
+                && update.getTimeSent() != null
+                && update.getPicture() != null;
     }
 }
