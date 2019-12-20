a1a11c6c061be0236357d880525f1bc7c819780d
Carl Antoine <carlantoine14@gmail.com>
2019-12-20 14:07:39 -0500

    LiveUpdate.java fix

--- app/src/main/java/org/httpsknighthacks/knighthacksandroid/Models/LiveUpdate.java
+++ app/src/main/java/org/httpsknighthacks/knighthacksandroid/Models/LiveUpdate.java
@@ -9,11 +9,13 @@ public class LiveUpdate {
     private Optional<String> mMessage;
     private Optional<String> mTimeSent;
     private Optional<String> mPicture;
+    // fix
     private Optional<String> mOptionalImage;
 
     public static final String MESSAGE_KEY = "message";
     public static final String TIME_SENT_KEY = "timeSent";
     public static final String PICTURE_KEY = "picture";
+    // fix
     public static final String OPTIONAL_IMAGE_KEY = "image";
 
     public LiveUpdate(JSONObject jsonObject) {
@@ -26,6 +28,7 @@ public class LiveUpdate {
             this.mMessage = Optional.empty();
             this.mTimeSent = Optional.empty();
             this.mPicture = Optional.empty();
+            // fix
             this.mOptionalImage = Optional.empty();
         }
     }
@@ -58,6 +61,7 @@ public class LiveUpdate {
         return mPicture;
     }
 
+    // fix
 
     public Optional<String> getImageOptional () {
         return mOptionalImage;
@@ -68,5 +72,6 @@ public class LiveUpdate {
                 && update.getTimeSentOptional().isPresent()
                 && update.getPictureOptional().isPresent()
                 && update.getImageOptional().isPresent();
+        // Fix
     }
 }
