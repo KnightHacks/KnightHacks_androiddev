3da006799dcec1f533204912f8e005a9c4cd5603
Kevin <rabinowitzkevin@gmail.com>
2020-01-08 19:52:40 -0500

    implement qr code scanner

--- app/src/main/java/org/httpsknighthacks/knighthacksandroid/Profile.java
+++ app/src/main/java/org/httpsknighthacks/knighthacksandroid/Profile.java
@@ -1,5 +1,6 @@
 package org.httpsknighthacks.knighthacksandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -30,10 +31,8 @@ public class Profile extends AppCompatActivity {
         scanQrCodeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // TODO : Add QR scan feature
-
-                Toast.makeText(Profile.this, "Not Successful",
-                        Toast.LENGTH_SHORT).show();
+                Intent newActivity = new Intent(view.getContext(), LiveBarcodeScanningActivity.class);
+                view.getContext().startActivity(newActivity);
             }
         });
     }
