62665c7eedd702f92cf69d3d94bb12c94a734e97
Carl Antoine <carlantoine14@gmail.com>
2020-02-05 18:10:24 -0500

    Fixed requested changes.

--- app/src/main/java/org/httpsknighthacks/knighthacksandroid/Profile.java
+++ app/src/main/java/org/httpsknighthacks/knighthacksandroid/Profile.java
@@ -13,16 +13,12 @@ import android.provider.MediaStore;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.bumptech.glide.Glide;
 
@@ -47,55 +43,12 @@ public class Profile extends AppCompatActivity {
 
         setContentView(R.layout.activity_profile);
 
-        CardView scanQrCodeButton = findViewById(R.id.scan_qr_code_button);
+        CardView scanQrCodeButton = findViewById(R.id.profile_scan_qr_code_button);
         scanQrCodeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent newActivity = new Intent(view.getContext(), LiveBarcodeScanningActivity.class);
                 view.getContext().startActivity(newActivity);
-            }
-        });
-
-        CardView showQRCodeBtn = findViewById(R.id.show_qr_btn);
-        showQRCodeBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        CardView myGroupsBtn = findViewById(R.id.my_groups_btn);
-        myGroupsBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        CardView settingsButton = findViewById(R.id.settings_button);
-        settingsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // TODO add settings fragment and portion
-
-                Toast.makeText(Profile.this, "No Settings page yet",
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        CardView loginEmailBtn = findViewById(R.id.email_login_btn);
-        loginEmailBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        CardView logoutBtn = findViewById(R.id.logout_btn);
-        logoutBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
             }
         });
 
