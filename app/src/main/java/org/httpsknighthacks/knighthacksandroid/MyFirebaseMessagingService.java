29259dd75779ae09ec44ee529728765fb9c8d71e
Carl Antoine <carlantoine14@gmail.com>
2020-02-20 19:47:52 -0500

    Added Notification Topics and User ability to turn them on/off

--- app/src/main/java/org/httpsknighthacks/knighthacksandroid/MyFirebaseMessagingService.java
+++ app/src/main/java/org/httpsknighthacks/knighthacksandroid/MyFirebaseMessagingService.java
@@ -1,6 +1,9 @@
 package org.httpsknighthacks.knighthacksandroid;
 
 import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.media.RingtoneManager;
 import android.util.Log;
 
 import androidx.core.app.NotificationCompat;
@@ -9,14 +12,21 @@ import androidx.core.app.NotificationManagerCompat;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import static android.app.Notification.DEFAULT_SOUND;
+
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private static final String TAG = "KnightHacksIsTheBest";
+    public static final String TOKEN_BROADCAST = "GOKnights";
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
 
-        String title = remoteMessage.getData().get("title");
-        String body = remoteMessage.getData().get("body");
+        Intent fullScreenIntent = new Intent(this, MainActivity.class);
+        PendingIntent fullScreenPendingIntent = PendingIntent.getActivity(this, 0,
+                fullScreenIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        String title = remoteMessage.getNotification().getTitle();
+        String body = remoteMessage.getNotification().getBody();
 
         // TODO(developer): Handle FCM messages here.
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
@@ -24,17 +34,28 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         super.onMessageReceived(remoteMessage);
 
-        Notification notification = new NotificationCompat.Builder(this, "MyNotifications")
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(this, "GENERAL")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .setContentTitle(title)
                 .setContentText(body)
                 .setSmallIcon(R.drawable.rover)
-                .setAutoCancel(true)
-                .build();
+                .setFullScreenIntent(fullScreenPendingIntent, true)
+                .setAutoCancel(true);
 
         NotificationManagerCompat manager = NotificationManagerCompat.from(getApplicationContext());
-        manager.notify(123, notification);
+        manager.notify(1, notification.build());
 
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
+    }
+
+    @Override
+    public void onNewToken(String token)
+    {
+        Log.d("myFirebaseId", "Refreshed token: " + token);
+
+        getApplicationContext().sendBroadcast(new Intent(TOKEN_BROADCAST));
+
+        // sendRegistrationToServer(token);
     }
 }
